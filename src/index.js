3c7860706e9c812cab3a50d88135338f997dcb29
pixz <tinycactusking@gmail.com>
2025-01-27 20:32:15 -0500

    Init webpack setup, everything connected and working

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import pizzaImage from "./pizza.jpg";
+
+const image = document.createElement("img");
+image.src = pizzaImage;
+document.body.appendChild(image);
+
+console.log("hello wrld");
